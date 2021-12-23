7105058a255737371df2ccfffb65c695fc903dd1
Nikita Shaposhnikov <topru123@gmail.com>
2021-12-23 12:32:53 +0300

    refactoring and copyright update

--- src/main/kotlin/com/exactpro/th2/codec/http/HttpPipelineCodecFactory.kt
+++ src/main/kotlin/com/exactpro/th2/codec/http/HttpPipelineCodecFactory.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020-2022 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.exactpro.th2.codec.http
 
 import com.exactpro.th2.codec.api.IPipelineCodec
@@ -6,18 +22,10 @@ import com.exactpro.th2.codec.api.IPipelineCodecFactory
 import com.exactpro.th2.codec.api.IPipelineCodecSettings
 
 class HttpPipelineCodecFactory : IPipelineCodecFactory {
-
     override val settingsClass: Class<out IPipelineCodecSettings> = HttpPipelineCodecSettings::class.java
     override val protocol: String = PROTOCOL
-
-    override fun init(pipelineCodecContext: IPipelineCodecContext) {
-
-    }
-
-    override fun create(settings: IPipelineCodecSettings?): IPipelineCodec {
-        return HttpPipelineCodec()
-    }
-
+    override fun init(pipelineCodecContext: IPipelineCodecContext) = Unit
+    override fun create(settings: IPipelineCodecSettings?): IPipelineCodec = HttpPipelineCodec()
 
     companion object {
         const val PROTOCOL = "http"
