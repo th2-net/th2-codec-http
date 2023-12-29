8525c1afd2a99bfc4d5aea77f6a142181f43be21
Nikita Smirnov <46124551+Nikita-Smirnov-Exactpro@users.noreply.github.com>
2023-12-29 14:07:22 +0400

    [Th2 5143] problem when codec publishes event for book `A` with attached messages for book `B` (#16)
    * [TH2-4934] th2 transport protocol support
    
    ---------
    
    Co-authored-by: Oleg Smelov <oleg.smelov@exactprosystems.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# HTTP Codec v0.2.0
+# HTTP Codec v0.4.0
 
 This microservice can encode and decode HTTP messages
 
@@ -27,32 +27,33 @@ This codec processes parsed messages with `http` protocol in metadata field
     1. HTTP message - `Message` with `Request` or `Response` type (protocol = `http`)
     2. HTTP message body - `RawMessage` with a raw message body (if present)
 
-If decoded message was an HTTP request, message body metadata would contain `method` and `uri` properties with HTTP request method name and URI respectively
+If decoded message was an HTTP request, message body metadata would contain `method` and `uri` properties with HTTP
+request method name and URI respectively
 
 ## Message types
 
 * Request
 
-|Field|Type|Description|
-|:---:|:---:|:---:|
-|method|String|HTTP method name (e.g. GET, POST, etc.)|
-|uri|String|Request URI (e.g. /some/request/path?param1=value1&param2=value2...)|
-|headers|List\<Header>|HTTP headers (e.g. Host, Content-Length, etc.)|
+|  Field  |     Type      |                             Description                              |
+|:-------:|:-------------:|:--------------------------------------------------------------------:|
+| method  |    String     |               HTTP method name (e.g. GET, POST, etc.)                |
+|   uri   |    String     | Request URI (e.g. /some/request/path?param1=value1&param2=value2...) |
+| headers | List\<Header> |            HTTP headers (e.g. Host, Content-Length, etc.)            |
 
 * Response
 
-|Field|Type|Description|
-|:---:|:---:|:---:|
-|statusCode|String|HTTP status code (e.g. 200, 403, 500, etc)|
-|reason|String|HTTP status reason (e.g. OK, Forbidden, Internal Server Error, etc.)|
-|headers|List\<Header>|HTTP headers (e.g. Set-Cookie, Content-Length, etc.)|
+|   Field    |     Type      |                             Description                              |
+|:----------:|:-------------:|:--------------------------------------------------------------------:|
+| statusCode |    String     |              HTTP status code (e.g. 200, 403, 500, etc)              |
+|   reason   |    String     | HTTP status reason (e.g. OK, Forbidden, Internal Server Error, etc.) |
+|  headers   | List\<Header> |         HTTP headers (e.g. Set-Cookie, Content-Length, etc.)         |
 
 * Header
 
-|Field|Type|Description|
-|:---:|:---:|:---:|
-|name|String|HTTP header name|
-|value|String|HTTP header value|
+| Field |  Type  |    Description    |
+|:-----:|:------:|:-----------------:|
+| name  | String | HTTP header name  |
+| value | String | HTTP header value |
 
 ## Deployment via `infra-mgr`
 
@@ -119,16 +120,20 @@ spec:
       enabled: false
 ```
 
-### Gradle metadata note
-ignoreGradleMetadataRedirection is used for sonatype because Sailfish dependencies have constrains that interfere with BOM,
-so we exclude Gradle metadata for these repositories.
-
-It's been verified that Sailfish itself is compatible with versions from BOM and therefore safe to use.
-
-
 # Release notes
 
+## 0.4.0
+
++ th2 transport protocol support
++ Updated bom: `4.5.0-dev`
++ Updated common: `5.7.2-dev`
++ Updated common-utils: `2.2.2-dev`
++ Updated codec: `5.4.1-dev`
++ Updated kotlin: `1.8.22`
++ Removed sailfish-utils
+
 ## 0.3.0
+
 * th2-common upgrade to `3.44.0`
 * th2-bom upgrade to `4.1.0`
 * th2-codec upgrade to `4.7.6`
--- src/test/kotlin/com/exactpro/th2/codec/http/EncodeTest.kt
+++ src/test/kotlin/com/exactpro/th2/codec/http/EncodeTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2022 Exactpro (Exactpro Systems Limited)
+ * Copyright 2020-2023 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,14 +16,20 @@
 
 package com.exactpro.th2.codec.http
 
+import com.exactpro.th2.codec.api.impl.ReportingContext
 import com.exactpro.th2.codec.http.HttpPipelineCodecFactory.Companion.PROTOCOL
-import com.exactpro.th2.common.grpc.AnyMessage
-import com.exactpro.th2.common.grpc.Message
-import com.exactpro.th2.common.grpc.MessageGroup
+import com.exactpro.th2.common.grpc.AnyMessage as ProtoAnyMessage
+import com.exactpro.th2.common.grpc.Message as ProtoMessage
+import com.exactpro.th2.common.grpc.MessageGroup as ProtoMessageGroup
 import com.exactpro.th2.common.message.addField
 import com.exactpro.th2.common.message.messageType
+import com.exactpro.th2.common.schema.message.impl.rabbitmq.transport.EventId
+import com.exactpro.th2.common.schema.message.impl.rabbitmq.transport.MessageGroup
+import com.exactpro.th2.common.schema.message.impl.rabbitmq.transport.ParsedMessage
+import com.exactpro.th2.common.schema.message.impl.rabbitmq.transport.RawMessage
 import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
+import java.time.Instant
 
 class EncodeTest {
 
@@ -32,7 +38,7 @@ class EncodeTest {
         val eventID = "123"
 
         val codec = HttpPipelineCodec()
-        val message = Message.newBuilder().apply {
+        val message = ProtoMessage.newBuilder().apply {
             messageType = HttpPipelineCodec.REQUEST_MESSAGE
             addField(HttpPipelineCodec.URI_FIELD, "/test")
             addField(HttpPipelineCodec.METHOD_FIELD, "GET")
@@ -40,9 +46,9 @@ class EncodeTest {
             metadataBuilder.protocol = PROTOCOL
         }
 
-        val messageGroup = MessageGroup.newBuilder().addMessages(AnyMessage.newBuilder().setMessage(message).build()).build()
+        val messageGroup = ProtoMessageGroup.newBuilder().addMessages(ProtoAnyMessage.newBuilder().setMessage(message).build()).build()
 
-        val encodedEventID = codec.encode(messageGroup).getMessages(0).rawMessage.parentEventId
+        val encodedEventID = codec.encode(messageGroup, ReportingContext()).getMessages(0).rawMessage.parentEventId
 
         assertEquals(eventID, encodedEventID.id)
     }
@@ -52,7 +58,7 @@ class EncodeTest {
         val eventID = "123"
 
         val codec = HttpPipelineCodec()
-        val message = Message.newBuilder().apply {
+        val message = ProtoMessage.newBuilder().apply {
             messageType = HttpPipelineCodec.RESPONSE_MESSAGE
             addField(HttpPipelineCodec.STATUS_CODE_FIELD, "200")
             addField(HttpPipelineCodec.REASON_FIELD, "OK")
@@ -60,11 +66,62 @@ class EncodeTest {
             metadataBuilder.protocol = PROTOCOL
         }
 
-        val messageGroup = MessageGroup.newBuilder().addMessages(AnyMessage.newBuilder().setMessage(message).build()).build()
-
-        val encodedEventID = codec.encode(messageGroup).getMessages(0).rawMessage.parentEventId
+        val messageGroup = ProtoMessageGroup.newBuilder().addMessages(ProtoAnyMessage.newBuilder().setMessage(message).build()).build()
+        val encodedEventID = codec.encode(messageGroup, ReportingContext()).getMessages(0).rawMessage.parentEventId
 
         assertEquals(eventID, encodedEventID.id)
     }
 
+    @Test
+    fun `parent event id test - request (transport)`() {
+        val eventID = "123"
+
+        val codec = HttpPipelineCodec()
+        val message = ParsedMessage(
+            eventId = EventId(eventID, "book_1", "scope_1", Instant.now()),
+            type = HttpPipelineCodec.REQUEST_MESSAGE,
+            protocol = PROTOCOL,
+            body = mapOf(
+                HttpPipelineCodec.URI_FIELD to "/test",
+                HttpPipelineCodec.METHOD_FIELD to "GET"
+            )
+        )
+
+        val messageGroup = MessageGroup(listOf(message))
+        val encodedGroup = codec.encode(messageGroup, ReportingContext())
+        val encodedMessage = encodedGroup.messages[0] as RawMessage
+        val encodedEventID = encodedMessage.eventId
+
+        assertEquals(1, encodedGroup.messages.size)
+        assertEquals(PROTOCOL, encodedMessage.protocol)
+        assertEquals(22, encodedMessage.body.readableBytes())
+        assertEquals(eventID, encodedEventID?.id)
+    }
+
+    @Test
+    fun `parent event id test - response (transport)`() {
+        val eventID = "123"
+
+        val codec = HttpPipelineCodec()
+        val message = ParsedMessage(
+            eventId = EventId(eventID, "book_1", "scope_1", Instant.now()),
+            type = HttpPipelineCodec.RESPONSE_MESSAGE,
+            protocol = PROTOCOL,
+            body = mapOf(
+                HttpPipelineCodec.STATUS_CODE_FIELD to "200",
+                HttpPipelineCodec.REASON_FIELD to "OK"
+            )
+        )
+
+        val messageGroup = MessageGroup(listOf(message))
+        val encodedGroup = codec.encode(messageGroup, ReportingContext())
+
+        val encodedMessage = encodedGroup.messages[0] as RawMessage
+        val encodedEventID = encodedMessage.eventId
+
+        assertEquals(1, encodedGroup.messages.size)
+        assertEquals(PROTOCOL, encodedMessage.protocol)
+        assertEquals(19, encodedMessage.body.readableBytes())
+        assertEquals(eventID, encodedEventID?.id)
+    }
 }
